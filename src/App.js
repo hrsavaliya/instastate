a2d9b9356164d9bd4585f852f89185a1450696ff
Parth Dhameliya <parthdhameliya266@gmail.com>
2021-03-26 14:30:51 +0530

    ui and changes && search issue fix

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React from "react";
 import {Input} from "antd";
 import Odometer from 'react-odometerjs';
 import {getSearchData,intervalgetSearchData} from "../../instastate/src/Actions/App";
+import pic1 from "../src/Assets/images/newinsta.webp"
+import pic2 from "../src/Assets/images/newfacebook.webp"
+import pic3 from "../src/Assets/images/newtwitter.png"
+
 
 
 class App extends React.Component{
@@ -11,6 +15,7 @@ class App extends React.Component{
         this.state = {
             userData: {},
             searchData : "",
+            duplicateSearchData:"",
             selectedUserName: ""
         }
 
@@ -29,7 +34,7 @@ class App extends React.Component{
                 const min = 1111111;
                 const max = 9999999;
                 const rand =  min + (Math.random() * (max-min));
-                const data = await intervalgetSearchData(this.state.userData.userName || this.state.searchData || Random, rand)
+                const data = await intervalgetSearchData(this.state.userData.userName || this.state.duplicateSearchData || Random, rand)
                  data.userName= this.state.searchData
                 console.log("sadas", data)
                 this.setState({userData: {data}},() => {
@@ -53,6 +58,7 @@ class App extends React.Component{
         const {value} = e.target;
         this.setState({searchData : value})
         if(e.key === 'Enter' || e.keyCode === 13){
+            this.setState({duplicateSearchData: value})
             this.onSubmit();
         }
     };
@@ -86,7 +92,7 @@ class App extends React.Component{
                                        data-delay={200}
                                        data-tooltip="Click to load a random user!"><span>Insta</span>statistics</a>
                                     {/*<span><SearchOutlined style={{fontSize:16,color:"black",background:"white"}}/></span>*/}
-                                    <Input style={{background:"white", width:"25%",height:"25%", padding:10}} className="ExportsCustomers customersDropdown" name="app"
+                                    <Input style={{background:"white", width:"30%", padding:3}} className="ExportsCustomers customersDropdown" name="app"
                                            onChange={this.searchName}
                                            value={searchData}
                                            placeholder="Enter User Name"
@@ -99,13 +105,11 @@ class App extends React.Component{
                 <div className="container">
                     <div style={{marginTop: -365}}>
                         <div className="user-information">
-                            <img id="profile-picture" src={data && data.avatar  || "./Assets/images/default_avatar.jpg"}
-                                 className="circle responsive-img"/>
                             <div className="username">
-                                <p className="header-subtitle">Live Instagram Statistics</p>
-                                <p><span id="profile-name">{data && data.username || this.state.selectedUserName}</span> <span id="verified" className="hide"><i
-                                    className="material-icons tooltipped" data-position="right" data-delay={50}
-                                    data-tooltip="Verified User">verified_user</i></span></p>
+                                {/*<p className="header-subtitle">Live Instagram Statistics</p>*/}
+                                {/*<p><span id="profile-name">{data && data.username || this.state.selectedUserName}</span> <span id="verified" className="hide"><i*/}
+                                {/*    className="material-icons tooltipped" data-position="right" data-delay={50}*/}
+                                {/*    data-tooltip="Verified User">verified_user</i></span></p>*/}
                             </div>
                         </div>
                         <div className="searchresults content">
@@ -113,48 +117,83 @@ class App extends React.Component{
                             </ul>
                         </div>
                         <div className="content center">
-                            <p className="subtitle">Followers</p>
-                            <div id="follower-count"><Odometer value={data && data.followerCount || 0} format="(,ddd),dd" /></div>
-                        </div>
-                        <div className="content center">
                             <div className="row">
-                                <div className="col s12 m6">
-                                    <p className="subtitle"><i className="material-icons">people</i>&nbsp;&nbsp;Following
-                                    </p>
-                                    <div id="following-count">{data && data.followingCount || 0}</div>
+                                <div className="col">
+                            <div className="username">
+                            <img style={{borderRadius:"25px",height:320}} id="profile-picture" src={(data && data.avatar)  || "./Assets/images/default_avatar.jpg"}
+                                 className="circle responsive-img content left"/>
+                            </div>
                                 </div>
-                                <div className="col s12 m6">
+                                <div className="col">
+                            <div>
+                                <p className="header-subtitle">Live Instagram Statistics</p>
+                                <p><span id="profile-name" style={{color:"#000000"}}>{(data && data.username) || this.state.selectedUserName}</span> <span id="verified" className="hide"><i
+                                    className="material-icons tooltipped" data-position="right" data-delay={50}
+                                    data-tooltip="Verified User">verified_user</i></span></p>
+                            </div>
+                            <p className="subtitle">Followers</p>
+                            <div id="follower-count"><Odometer value={(data && data.followerCount) || 0} format="(,ddd),dd" /></div>
+                                    <p style={{display:"inline-flex"}}>
+                                <span style={{marginRight:25}}>
+                                    <p  className="subtitle"><i className="material-icons">people</i>&nbsp;&nbsp;Following
+                                    </p>
+                                    <div  id="following-count">{(data && data.followingCount) || 0}</div>
+                                </span>
+                                <span>
                                     <p className="subtitle"><i className="material-icons">photo_camera</i>&nbsp;&nbsp;Posts
                                     </p>
                                     <div id="posts-count">{data && data.mediaCount || 0}</div>
+                                </span>
+                                    </p>
                                 </div>
                             </div>
                         </div>
                         <div className="content center">
                             <div className="row">
                                 <div className="col s12">
-                                    <p className="subtitle">Promoted accounts</p>
+                                    <p className="subtitle"><i className="material-icons">description</i>&nbsp;&nbsp;<span
+                                        id="about-title">About {data && data.username || "User"}
+</span>
+                                    </p>
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="col s12">
-                                    <p>Want to promote your account too? Visit the <a href="https://shop.evolve.social/">Evolve
-                                        Social
-                                        Promotions</a> shop for more information.</p>
+                                    <p id="about-section"><em>{data && data.biography || "No Data Available"}</em></p>
                                 </div>
                             </div>
                         </div>
                         <div className="content">
                             <div className="row">
-                                <div className="col-sm-6">
-                                    <p className="subtitle"><i className="material-icons">description</i>&nbsp;&nbsp;<span
-                                        id="about-title">About {data && data.username || "User"}
-</span>
+                                <div className="col-sm-12">
+                                    <p className="subtitle"><i className="material-icons">share</i>&nbsp;&nbsp;Share</p>
+                                    <p className="social-share" style={{marginTop:20}}>
+                                    <a style={{
+                                        marginTop: 10,
+                                        borderRadius: 15,
+                                        marginRight:15
+                                    }}> <img src={pic1} style={{height:50,width:50}}/></a>
+                                        <a style={{
+                                            marginTop: 10,
+                                            borderRadius: 15,
+                                            marginRight:15
+                                        }}> <img src={pic2} style={{height:50,width:50}}/></a>
+                                        <a style={{
+                                            marginTop: 10,
+                                            borderRadius: 15
+                                        }}> <img src={pic3} style={{height:50,width:50}}/></a>
                                     </p>
-                                    <p id="about-section"><em>{data && data.biography || "No Data Available"}</em></p>
                                     <div className="divider"/>
-                                    <p>Website: <a id="about-section-url" href="#">http://themessistore.com/</a></p>
-                                    <p>Username: <span id="about-section-username">leomessi</span></p>
+                                    {/*<p>*/}
+                                    {/*    Click on the link below to copy it to your clipboard so you can share it with*/}
+                                    {/*    others!*/}
+                                    {/*    <input type="text" name="url" placeholder="N/A" id="shareURL"*/}
+                                    {/*           onClick="this.select(); copyShareLink('site');" readOnly/>*/}
+                                    {/*</p>*/}
+
+                                </div>
+                                <div className="col-sm-12">
+                                    <div className="divider"/>
                                     <p className="subtitle"><i className="material-icons">domain</i>&nbsp;&nbsp;Follow Us
                                     </p>
                                     <p style={{display: "flex", marginTop: 15}}><a id="about-section-url" href="#">
@@ -182,8 +221,6 @@ class App extends React.Component{
                                         }}
                                                                       className="fa fa-twitter"/>Follow @BjarnBronsveld</a>
 
-
-                                    </p>
                                     <a style={{
                                         height: 24,
                                         fontSize: 10,
@@ -204,6 +241,7 @@ class App extends React.Component{
                                     marginTop: -10, verticalAlign: "sub",
                                 }}
                                                                className="fa fa-like"/>Delen</a>
+                                    </p>
                                     <div className="fb-like" data-href="https://instastatistics.com"
                                          data-layout="button_count"
                                          data-action="like" data-size="small" data-show-faces="true" data-share="true">
@@ -216,34 +254,7 @@ class App extends React.Component{
                                     {/*<i className="fa fa-twitter"><i className="fa fa-facebook"><i className="fa fa-instagram">*/}
                                     {/*</i></i></i>*/}
                                 </div>
-                                <div className="col-sm-6">
-                                    <p className="subtitle"><i className="material-icons">share</i>&nbsp;&nbsp;Share</p>
-                                    <p className="social-share"><a style={{
-                                        marginTop: 10,
-                                        borderRadius: 15
-                                    }} className="waves-effect waves-dark btn btn-rounded twitter"
-                                                                   onClick="RIFC.share('twtr');"><i
-                                        className="fa fa-twitter">&nbsp;&nbsp;Share
-                                        on Twitter</i></a></p><i className="">
-                                    <p className="social-share"><a style={{borderRadius: 15}}
-                                                                   className="waves-effect waves-dark btn btn-rounded facebook"
-                                                                   onClick="RIFC.share('fb');"><i
-                                        className="fa fa-facebook">&nbsp;&nbsp;Share
-                                        on Facebook</i></a></p><i className="">
-                                    <p className="social-share"><a style={{borderRadius: 15}}
-                                                                   className="waves-effect waves-dark btn btn-rounded instagram"
-                                                                   onClick="RIFC.share('ig');"><i
-                                        className="fa fa-instagram">&nbsp;&nbsp;Follow
-                                        on Instagram</i></a></p><i className="">
-                                    <div className="divider"/>
-                                    <p>
-                                        Click on the link below to copy it to your clipboard so you can share it with
-                                        others!
-                                        <input type="text" name="url" placeholder="N/A" id="shareURL"
-                                               onClick="this.select(); copyShareLink('site');" readOnly/>
-                                    </p>
 
-                                </i></i></i></div>
                             </div>
                         </div>
                     </div>
